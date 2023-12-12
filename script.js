cb7fb9af0150df251661c677e2dc64e68c6f772e
Glen112 <escobar.glen@gmail.com>
2023-12-12 10:47:07 -0300

    alurabooks-mobile-carrossel

---
+++ script.js
@@ -0,0 +1,23 @@
+//slider carrossel
+    const swiper = new Swiper('.swiper', {
+      spaceBetween: 10,
+      slidesPerView:3,
+  
+
+    // If we need pagination
+     pagination: {
+       el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+     },
+
+    // And if we need scrollbar
+  //   scrollbar: {
+   //    el: '.swiper-scrollbar',
+   //  },
+  });
+
